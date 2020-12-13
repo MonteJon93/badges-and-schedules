8752e3ec397114107e8a6824e771e760153a6c07
Jonathan Montesdeoca <JMontesdeoca93@gmail.com>
2020-12-12 22:31:22 -0500

    commit tree 0ef542bc03fd403503e26d5b984db9e157b667ed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+names.map.with_index(0) {|name, i| 
+    "Hello, #{name}! You'll be assigned to room #{i+1}!"}
+end
+
+def printer(names)
+    batch_badge_creator(names).map do |badge|
+         puts badge
+    end
+   
+    assign_rooms(names).map do |message|
+        puts message
+    end
+
+end 
